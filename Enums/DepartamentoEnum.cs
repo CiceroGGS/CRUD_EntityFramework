a7d306cd3d63d67d3696c9e013b4abd460d80a9f
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-18 14:44:38 -0300

    feat: Adicinado JsonConverter nos enums para melhor visualizacao

--- Enums/DepartamentoEnum.cs
+++ Enums/DepartamentoEnum.cs
@@ -1,5 +1,8 @@
-﻿namespace WebApiFuncionariosCRUD.Enums
+﻿using System.Text.Json.Serialization;
+
+namespace WebApiFuncionariosCRUD.Enums
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public enum DepertamentoEnum
     {
         RH,
