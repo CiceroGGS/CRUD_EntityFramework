305ce6aeb672cb410aab487a1ac51c06e3d38144
CiceroGGS <cicerog.silvestre@gmail.com>
2025-04-19 07:57:11 -0300

    feat: adcionar CORS para permitir .NET a fazer chamadas fora do dominio

--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
-using Microsoft.EntityFrameworkCore;
-using WebApiFuncionariosCRUD.DataContext;
-using WebApiFuncionariosCRUD.Service;
+﻿using Microsoft.EntityFrameworkCore;
 using WebApiFuncionariosCRUD.DataContext;
 using WebApiFuncionariosCRUD.Service;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") 
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IFuncionarioInterface, FuncionarioService>();
@@ -21,12 +27,15 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Middleware para desenvolvimento
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+// ⚠️ A ORDEM IMPORTA: UseCors vem antes do UseAuthorization
+app.UseCors("AllowAngularApp");
 
 app.UseHttpsRedirection();
 
--- obj/Debug/net8.0/WebApiFuncionariosCRUD.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApiFuncionariosCRUD.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApiFuncionariosCRUD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15a822f9b99b811beddf4945a7d02423a3cda50b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63514711c2df2a1fade3f970645d87d2b02ed4a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApiFuncionariosCRUD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApiFuncionariosCRUD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
